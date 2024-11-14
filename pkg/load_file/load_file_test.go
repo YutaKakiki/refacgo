d6b102fe24232517bcd4e4ddb77e1706e079a6dd
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-11-15 03:12:49 +0900

    LoadFileを内部用と外部用に分割

--- pkg/load_file/load_file_test.go
+++ pkg/load_file/load_file_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestLoadFile(t *testing.T) {
+func TestLoadInternal(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
 		name     string
@@ -25,7 +25,37 @@ func TestLoadFile(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			got, err := LoadFile(tt.filepath)
+			got, err := LoadInternal(tt.filepath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("utils.LoadFile() error = %v ,wantErr = %v", err, tt.wantErr)
+			}
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("loadFile return byte mismatch(-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestLoadExternal(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name     string
+		filepath string
+		want     []byte
+		wantErr  bool
+	}{
+		{
+			name:     "ファイルを読み込み、正しいバイトスライスを返す",
+			filepath: "./testdata/sample.txt",
+			want:     []byte("This is Sample File.\n"),
+			wantErr:  false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := LoadExternal(tt.filepath)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("utils.LoadFile() error = %v ,wantErr = %v", err, tt.wantErr)
 			}
