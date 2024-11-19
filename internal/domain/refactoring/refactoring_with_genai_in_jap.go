1eb34b072feb8813a840e1da1c0fccb3ca8175ab
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-11-20 02:25:52 +0900

    RefactoringWithGenAiInJapの実装、テスト完了

---
+++ internal/domain/refactoring/refactoring_with_genai_in_jap.go
@@ -0,0 +1,31 @@
+package refactoring
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+
+	"github.com/kakky/refacgo/internal/domain"
+)
+
+type RefactoringWithGenAiInJap struct {
+	genAI domain.GenAI
+}
+
+func NewRefactoringWithGenAiInJap(genAI domain.GenAI) *RefactoringWithGenAiInJap {
+	return &RefactoringWithGenAiInJap{
+		genAI: genAI,
+	}
+}
+
+//go:embed instruction_text/genai_instruction_in_jap.txt
+var instructionInJap string
+
+func (rf *RefactoringWithGenAiInJap) Refactor(ctx context.Context, src []byte, filename string, ch chan<- string) error {
+	fmt.Println(instructionInJap)
+	prompt := fmt.Sprintf("このファイル名は %q です。\n\n%v\n\n", filename, instructionInJap)
+	if err := rf.genAI.QueryResuluts(ctx, src, prompt, ch); err != nil {
+		return err
+	}
+	return nil
+}
