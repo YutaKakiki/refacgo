0b8105f322ba24bf664bb4b2794180d060e0ad31
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-11-20 02:26:53 +0900

    refac関連のプレゼンター層のモックを作成

--- internal/presenter/refac_overwriter_interface.go
+++ internal/presenter/refac_overwriter_interface.go
@@ -1,5 +1,6 @@
 package presenter
 
+//go:generate mockgen -package=presenter -source=./refac_overwriter_interface.go -destination=./refac_overwriter_mock.go
 type RefacOverWriter interface {
 	OverWrite(filename string, src string) error
 	OverWriteWithHeaderComment(filename string, src string) error
--- internal/presenter/refac_printer_interface.go
+++ internal/presenter/refac_printer_interface.go
@@ -1,5 +1,7 @@
 package presenter
 
+//go:generate mockgen -package=presenter -source=./refac_printer_interface.go -destination=./refac_printer_mock.go
+
 type RefacPrinter interface {
 	Print(text ...string)
 }
